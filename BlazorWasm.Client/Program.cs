5bf771c801f1938f6a136438e80308077d75d649
Narayan Krishna Shrestha <nk_shres@hotmail.com>
2024-05-27 09:56:01 -0500

    Add project files.

---
+++ BlazorWasm.Client/Program.cs
@@ -0,0 +1,25 @@
+using Blazored.SessionStorage;
+using BlazorWasm.Client;
+using BlazorWasm.Client.Handlers;
+using BlazorWasm.Client.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddTransient<AuthenticationHandler>();
+
+string serverUrl = builder.Configuration["ServerUrl"] ?? "";
+
+builder.Services.AddHttpClient("ApiEndPoint")
+    .ConfigureHttpClient(c => c.BaseAddress = new Uri(serverUrl))
+    .AddHttpMessageHandler<AuthenticationHandler>();
+
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddSingleton<IAuthenticationService, AuthenticationService>();
+
+builder.Services.AddBlazoredSessionStorageAsSingleton();
+
+await builder.Build().RunAsync();
